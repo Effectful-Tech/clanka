e36321d3740ab993fae46a23a2d2984d9fdae690
Tim Smart <hello@timsmart.co>
2026-03-07 20:19:56 +1300

    lalph

---
+++ scripts/worktree-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+direnv allow
+corepack install
+pnpm install
+
+git clone https://github.com/effect-ts/effect-smol.git --depth 1 .repos/effect
+git clone https://github.com/effect-ts/content.git --depth 1 .repos/content
+git clone https://github.com/anomalyco/opencode.git --depth 1 .repos/opencode
